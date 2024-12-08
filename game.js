38d847f911e7a571acfad9c087645eb5d11be1e3
alizaiter777 <alizaiter777@gmail.com>
2024-12-09 01:06:26 +0200

    add event when mouseover id=end then call function endgame which is win

--- game.js
+++ game.js
@@ -37,6 +37,7 @@ function loadPage(){
     boundaries = document.getElementsByClassName("boundary");
     
     start.addEventListener("click", startGame);
+    end.addEventListener("mouseover", endGame);
     for(let i = 0; i < boundaries.length; i++){
         boundaries[i].addEventListener("mouseover", gameOver);
     }
