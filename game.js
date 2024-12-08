ceecfa9e8279bc85e9b84f4fe3c2fd684ecdc3b5
alizaiter777 <alizaiter777@gmail.com>
2024-12-09 00:34:21 +0200

    generate startGame function contain for loop make all boundries start with white background color

--- game.js
+++ game.js
@@ -4,12 +4,17 @@ let start;
 let boundaries;
 
 
+function startGame(){
+    for(let i = 0; i < boundaries.length; i++)
+        boundaries[i].style.backgroundColor = "#eeeeee"; 
+}
 
 function loadPage(){
     end = document.getElementById("end");
     start = document.getElementById("start");
     boundaries = document.getElementsByClassName("boundary");
-
+    
+    
 
     
 }
