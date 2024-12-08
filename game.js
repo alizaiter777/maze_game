1a96b3c2cf249e82c13e9c4a342ba34e8f161be1
alizaiter777 <alizaiter777@gmail.com>
2024-12-09 01:04:25 +0200

    generate endGame function that change boundries background color to green

--- game.js
+++ game.js
@@ -8,16 +8,21 @@ let boundaries;
 document.addEventListener("DOMContentLoaded", loadPage);
 
 
+function endGame(){
+    if(is_game_running){
+        for(let i = 0; i < boundaries.length; i++)
+            boundaries[i].style.backgroundColor = "rgb(113 225 141)"; 
+    }
+}
 
 
 function gameOver(){
     if(is_game_running){
         for(let i = 0; i < boundaries.length; i++)
-            boundaries[i].style.backgroundColor = "rgb(243, 159, 159)"; 
-       
-         
+            boundaries[i].style.backgroundColor = "rgb(243, 159, 159)";  
     }
 }
+
 
 function startGame(){
     is_game_running=true;
@@ -25,11 +30,11 @@ function startGame(){
         boundaries[i].style.backgroundColor = "#eeeeee"; 
 }
 
+
 function loadPage(){
     end = document.getElementById("end");
     start = document.getElementById("start");
     boundaries = document.getElementsByClassName("boundary");
-    
     
     start.addEventListener("click", startGame);
     for(let i = 0; i < boundaries.length; i++){
